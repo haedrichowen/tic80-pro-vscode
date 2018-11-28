52e6bc905b9a2aada7e16b6f748a7c814ba6706d
Justin Wash <trifectuh@icloud.com>
2018-11-27 19:23:35 -0600

    init pro

--- README.md
+++ README.md
@@ -1,69 +1,38 @@
-# tic80-vscode 
+# tic80-pro-vscode
 
-This is a extension for running lua files in tic80  for mac (and windows soon but should be works with path configuration)
-
+This is a fork of tic80-vscode meant for the Pro version of TIC-80
 
 ![vscodetic80](images/vscodetic80170.gif)
 
 ## Install
 
-Install tic80-Vscode plug-in
+Install tic80-pro-vscode
 
-Go in settings
-in your user workspace
-````
+Settings
+
+```
 {
-    "Tic80.executablePath": "/Applications/tic.app/Contents/MacOS/tic",
-    "Tic80.executablePathCard": "/Users/YourUserName/Library/Application Support/TIC-80/0.27.1 dev"
+    "Tic80.executablePath": "/Applications/tic.app/Contents/MacOS/tic"
 }
-````
-!!! replace YourUserName by your real username.
+```
 
-* `Tic80.executablePath`: By default it is  "/Applications/tic.app/Contents/MacOS/tic"
-if your executable is in Application. If not, get the path of your tic application
-
-* `Tic80.executablePathCard`:
-It is the folder where your cards with *.tic are  saved. If you want find it type "folder" in Tic80 application
-
-* `Tic80.showNameInformation`: Actived by default for sure the plugin works, for a comfortable develop desactive it :-)
+- `Tic80.executablePath`: By default it is "/Applications/tic.app/Contents/MacOS/tic80"
 
 ## Usage
 
-Open tic, create a new cart and save it.
-- Go in editor copy all code.
-- Go in your vscode workspace create a file with the same name of your card with extension lua.
-- Paste your code in file.
-- Make your changes.
-- Run tic with command Palette ="run tic80" or  cmd + R  
+Open tic, create a new cart and save it with the js, lua, etc flag e.g. "new js" > "save cart.js"
 
-- don't forget:
-     - when you save from vscode, the lua *.file is save but not the *.tic file.
-     - when you save from tic80 application, the *.tic file it's save but not the *.lua file.   
+- Open the cart in vscode and edit
+- Use the "tic" run command to start your cart in TIC-80 Pro from the command palette
+
+* don't forget to use "ctrl+S" to save any sprite or music changes in TIC-80 when switching between TIC-80 and VSCode
 
 ## Requirements
 
-Go to [tic80](https://tic.computer/) for tic application
-
-## Known Issues
-
-If Tic80 launch but you don't see sprites your `Tic80.executablePathCard` is no good.
-
-No tested on Windows, but just configuration path the should be alright. 
-more explication for configuration on windows soon.
-
-Open a issue for questions
-
+[TIC-80 Pro](https://tic.computer/)
 
 ## Release Notes
 
-- One instance when you start like the really tic80.
-- You can trace in the console.
-- Fix path file for windows
+### 0.1.0
 
-### 1.7.0
-One Instance, and trace in console better for debbuging ;-)
-
-
-### 1.0
-
-Initial release of VsCodeTIC80 plug in.
+Initial fork and release of tic80-pro-vscode.
--- extension.js
+++ extension.js
@@ -1,47 +1,41 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
-var TicTerminal = null;
+var vscode = require("vscode");
+var Tic80ProTerminal = null;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
+  let tic80ProSetup = vscode.workspace.getConfiguration("Tic80Pro");
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  var disposable = vscode.commands.registerTextEditorCommand(
+    "extension.runTic80Pro",
+    function(textEditor) {
+      // The code you place here will be executed every time your command is executed
 
-    let tic80Setup = vscode.workspace.getConfiguration('Tic80');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerTextEditorCommand('extension.runTic', function (textEditor) {
-        // The code you place here will be executed every time your command is executed
-   
-    textEditor.document.save();
+      textEditor.document.save();
 
-        const fileName = textEditor.document.uri.path;
-        var cartNameSplit = fileName.split('/');
-        var cartName= cartNameSplit[cartNameSplit.length-1].split('.')[0];
-        
-        const path = tic80Setup['executablePath'];
-        const pathGame = `${tic80Setup['executablePathCard']}/${cartName}.tic`
+      const cart = `${textEditor.document.uri.fsPath}`;
 
-        var args=[pathGame,"-code",fileName];  
+      const path = tic80ProSetup["executablePath"];
+      const args = [cart];
 
-    if(TicTerminal)
-        TicTerminal.dispose();
-     
-        TicTerminal = vscode.window.createTerminal('tic', path, args);
-        TicTerminal.show();
-    });
+      if (Tic80ProTerminal) Tic80ProTerminal.dispose();
 
+      Tic80ProTerminal = vscode.window.createTerminal("tic80pro", path, args);
+      Tic80ProTerminal.show();
+    }
+  );
 
-
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
